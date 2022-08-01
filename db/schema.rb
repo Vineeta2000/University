90211d8abe1cb8949fa692791bfbf972a5036f6a
Vineeta2000 <vineeta998114@gmail.com>
2022-08-01 15:14:50 +0530

    change database sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_07_06_092405) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "colleges", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -26,14 +29,14 @@ ActiveRecord::Schema.define(version: 2022_07_06_092405) do
     t.datetime "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.string "resource_type"
-    t.integer "resource_id"
+    t.bigint "resource_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
@@ -54,7 +57,7 @@ ActiveRecord::Schema.define(version: 2022_07_06_092405) do
     t.string "course"
     t.string "branch"
     t.integer "year"
-    t.integer "college_id"
+    t.bigint "college_id"
     t.integer "parent_id"
     t.index ["college_id"], name: "index_users_on_college_id"
     t.index ["email"], name: "index_users_on_email", unique: true
@@ -62,11 +65,13 @@ ActiveRecord::Schema.define(version: 2022_07_06_092405) do
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "role_id"
+    t.bigint "user_id"
+    t.bigint "role_id"
     t.index ["role_id"], name: "index_users_roles_on_role_id"
     t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
     t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
+  add_foreign_key "events", "users"
+  add_foreign_key "users", "colleges"
 end
